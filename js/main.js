45dc6449a18dd4eff311ad6936e1b427da376855
Adriano da Rosa Ferreira <adrianorosaferreira@gmail.com>
2019-02-07 15:53:01 -0200

    Adicionado ação de rotação de todos os ponteiros

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let secondHand = document.querySelector('.second-hand');
+let minHand = document.querySelector('.min-hand');
+let hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+    
+    const seconds = now.getSeconds();
+    const secondsDegrees = (( seconds / 60 ) * 360) + 90;
+
+    if (seconds != 0) {
+        console.log("Com trans:" + seconds);
+        secondHand.style.transition = `all 0.05s`;
+        secondHand.style.transitionTimingFunction = `cubic-bezier(0.1, 2.7, 0.58, 1)`;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    } else {
+        console.log("Sem trans:" + seconds);
+        secondHand.style.transition = `none`;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    }
+    
+    const minutes = now.getMinutes();
+    const minutesDegrees = (( minutes / 60 ) * 360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    
+    const hour = now.getHours();
+    const hourDegrees = (( hour / 12 ) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+}
+
+setInterval(setDate, 1000);
